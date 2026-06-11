0b8623158a4127e7eaffaa9f6665f2e284454776
arvajKhan <arvajkhan194@gmail.com>
2026-06-11 12:18:23 +0530

    add few file

---
+++ 02-Java-Core-OOp/HelloWord.java
@@ -0,0 +1,6 @@
+class HelloWord {
+    public static void main(String[] args) {
+        System.out.println((char)65 + "r" + "b" + (char)97 + "j");
+        
+    }
+}
---
+++ 02-Java-Core-OOp/printHelloWordwWthoutColon.java
@@ -0,0 +1,7 @@
+public class printHelloWordwWthoutColon{
+  public static void main(String[] args) {
+      if(System.out.printf("Arvaj") != null){
+
+      }
+  }
+}
