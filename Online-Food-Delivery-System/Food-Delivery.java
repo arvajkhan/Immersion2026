fdc60a1cc90d8fc15d386e71d357fbd35178a8fe
arvajKhan <arvajkhan194@gmail.com>
2026-06-16 08:50:45 +0530

    add few line code

---
+++ Online-Food-Delivery-System/Food-Delivery.java
@@ -0,0 +1,21 @@
+class foodItem{
+  int itemId;
+  String itemName;
+  int price;
+  int quantity;
+
+}
+
+
+class customer{
+  int customerId;
+  String  customerName;
+  int  mobileNumber;
+}
+
+foodItem(int itemId, String itemName, int price, int quantity){
+  this.itemId = itemId;
+  this.String = itemName;
+  this.price = price;
+  this.quantity = quantity;
+}
