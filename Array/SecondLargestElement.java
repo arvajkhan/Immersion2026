2a704d9b851a30686453bd990390fd63aa9e2f4f
arvajKhan <arvajkhan194@gmail.com>
2026-06-25 11:56:07 +0530

    Second largest ELement

---
+++ Array/SecondLargestElement.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        int[] secondLargest = {1, 2, 4, 1, 4, 2, 3};
+
+        Arrays.sort(secondLargest);
+        
+    }
+}
