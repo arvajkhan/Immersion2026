598b6103f05cc8e5bfda1609ba457b44ea27886e
Arvaj khan <arvajkhan194@gmail.com>
2026-07-02 11:26:39 +0530

    Remove comments and clean up SlidingWindow.java

--- SlidingWindow/sum/SlidingWindow.java
+++ SlidingWindow/sum/SlidingWindow.java
@@ -1,23 +1,16 @@
 public class SlidingWindow {
    public static void main(String[] args) {
-
         int[] arr = {2, 1, 5, 1, 3, 2};
         int k = 3;
-
-        // First window sum
         int windowSum = 0;
         for (int i = 0; i < k; i++) {
             windowSum += arr[i];
         }
-
         int maxSum = windowSum;
-
-        // Slide the window
         for (int i = k; i < arr.length; i++) {
             windowSum = windowSum - arr[i - k] + arr[i];
             maxSum = Math.max(maxSum, windowSum);
         }
-
         System.out.println("Maximum Sum = " + maxSum);
     }
 }
